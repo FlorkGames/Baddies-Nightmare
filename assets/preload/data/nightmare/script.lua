fee82d07927e66e95bc2665cddac34659c1e0f9c
FlorkGames <105853494+FlorkGames@users.noreply.github.com>
2022-12-23 20:13:12 -0300

    Add files via upload

--- assets/preload/data/nightmare/script.lua
+++ assets/preload/data/nightmare/script.lua
@@ -1,1 +1,5 @@
-
+function onCreate() 
+makeLuaText('watermark',"Optimized By Misha21220 [GD] ", 0, 2, 688); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 40);
+    addLuaText('watermark');
+end
